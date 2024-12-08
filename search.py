a52493b199f7cb42dd64dbd9a416080441d897ff
Nhan Thai <nathai@cpp.edu>
2024-12-07 23:37:16 -0800

    Update search.py

--- search.py
+++ search.py
@@ -8,7 +8,9 @@ BASE_URL = "https://www.cpp.edu"
 # Function to fetch the About section
 def fetch_about_section(soup):
     try:
-        about_header = soup.find('h2', string=re.compile(r'About\s+.*', re.IGNORECASE))
+        # about_header = soup.find('h2', string=re.compile(r'(About|Bio|Short|Biography|Overview)\s+.*', re.IGNORECASE))
+        about_header = soup.find(re.compile(r'(h2|h3|p)'), string=re.compile(r'(About|Bio|Short|Biography|Overview)', re.IGNORECASE))
+
         if about_header:
             parent_div = about_header.find_parent('div', class_='section-intro')
             if parent_div:
